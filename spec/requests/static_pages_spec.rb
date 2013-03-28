58cc7a14c746568e8a3750b321a4472eb73d9c4b
sethuav <sethu.varghese@gmail.com>
2013-03-28 18:35:00 +0400

    Finished exercises in Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App | " }
   
   describe "Home page" do
 
@@ -12,7 +14,7 @@ describe "StaticPages" do
   	it "should have the right title" do
   		visit '/static_pages/home'
   		page.should have_selector('title', 
-  			:text => "Ruby on Rails Tutorial Sample App | Home")
+  			:text => "#{base_title}Home")
   	end
   end
 
@@ -28,7 +30,7 @@ describe "StaticPages" do
 		it "should have the right title" do
 			visit '/static_pages/help'
 			page.should have_selector('title', 
-				:text	=> "Ruby on Rails Tutorial Sample App | Help")
+				:text	=> "#{base_title}Help")
 		end
 	end
 
@@ -42,8 +44,25 @@ describe "StaticPages" do
 		it "should have the right title" do
 			visit '/static_pages/about'
 			page.should have_selector('title', 
-				:text => "Ruby on Rails Tutorial Sample App | About")
+				:text => "#{base_title}About")
 		end
 	end
+
+	describe "Content page" do
+
+		it "should exist" do
+			visit '/static_pages/content'			
+		end
+
+    it "should have the content 'Content" do
+      visit '/static_pages/content'
+      page.should have_selector('h1', :text => 'Content')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/content'
+      page.should have_selector('title', :text => "#{base_title}Content")
+    end
+  end
 
 end
